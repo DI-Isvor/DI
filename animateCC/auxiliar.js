443f3c364485847957f3dd250ece1fe25ebf0d1e
DI-Isvor <53273613+DI-Isvor@users.noreply.github.com>
2019-12-12 04:58:58 -0300

    Add files via upload

---
+++ animateCC/auxiliar.js
@@ -0,0 +1,13 @@
+var n;
+var pos;
+
+function activeCheck(n, pos){
+    status[pos] = n;
+
+    // if(window.API != null){
+    //     suspendData = ScormProcessGetValue("cmi.suspend_data");
+    //     if(ScormProcessGetValue("cmi.suspend_data").indexOf(slideChecked[Reveal.getState().indexh]) < 0){
+    //         ScormProcessSetValue("cmi.suspend_data", suspendData + (slideChecked[Reveal.getState().indexh]));
+    //     }
+    // }    
+}
