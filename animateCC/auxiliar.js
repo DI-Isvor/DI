cd938beb8ebd2bf996fd8e4150a32637609cd8f6
DI-Isvor <53273613+DI-Isvor@users.noreply.github.com>
2019-12-12 05:03:16 -0300

    Update auxiliar.js

--- animateCC/auxiliar.js
+++ animateCC/auxiliar.js
@@ -1,13 +1,11 @@
 var n;
 var pos;
+var var n;;
 
 function activeCheck(n, pos){
-    status[pos] = n;
+    status[pos] = n; 
+}
 
-    // if(window.API != null){
-    //     suspendData = ScormProcessGetValue("cmi.suspend_data");
-    //     if(ScormProcessGetValue("cmi.suspend_data").indexOf(slideChecked[Reveal.getState().indexh]) < 0){
-    //         ScormProcessSetValue("cmi.suspend_data", suspendData + (slideChecked[Reveal.getState().indexh]));
-    //     }
-    // }    
+function consulta(v, pos){
+    v = status[pos]; 
 }
